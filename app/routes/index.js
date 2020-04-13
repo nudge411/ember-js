18c0e21a41a382c7aec9d2e7bd07b999ca675fa5
KimJoungUk <dnrl24a@gmail.com>
2020-04-13 17:11:51 +0900

    rental-page complete

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,26 +1,34 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-const COMMUNITY_CATEGORIES = [
-  'Condo',
-  'Townhouse',
-  'Apartment'
-];
+// const COMMUNITY_CATEGORIES = [
+//   'Condo',
+//   'Townhouse',
+//   'Apartment'
+// ];
 
 export default class IndexRoute extends Route {
-  async model() {
-    let response = await fetch('/api/rentals.json');
-    let { data } = await response.json();
+  // async model() {
+  //   let response = await fetch('/api/rentals.json');
+  //   let { data } = await response.json();
     
-    return data.map(model=> {
-      let { id, attributes } = model;
-      let type;
+  //   return data.map(model=> {
+  //     let { id, attributes } = model;
+  //     let type;
 
-      if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
-        type = 'Community';
-      } else {
-        type = 'Standalone';
-      }
-      return { id, type, ...attributes };
-    });
+  //     if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
+  //       type = 'Community';
+  //     } else {
+  //       type = 'Standalone';
+  //     }
+  //     return { id, type, ...attributes };
+  //   });
+  // }
+
+  @service store;
+  
+  async model() {
+    return this.store.findAll('rental');
   }
+
 }
--- app/routes/rental.js
+++ app/routes/rental.js
@@ -1,25 +1,32 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-const COMMUNITY_CATEGORIES = [
-  'Condo',
-  'Townhouse',
-  'Apartment'
-];
+// const COMMUNITY_CATEGORIES = [
+//   'Condo',
+//   'Townhouse',
+//   'Apartment'
+// ];
 
 export default class RentalRoute extends Route {
+  // async model(params) {
+  //   let response = await fetch(`/api/rentals/${params.rental_id}.json`);
+  //   let { data } = await response.json();
+
+  //   let { id, attributes } = data;
+  //   let type;
+
+  //   if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
+  //     type = 'Community';
+  //   } else {
+  //     type = 'Standalone';
+  //   }
+
+  //   return { id, type, ...attributes };
+  // }
+
+  @service store;
+
   async model(params) {
-    let response = await fetch(`/api/rentals/${params.rental_id}.json`);
-    let { data } = await response.json();
-
-    let { id, attributes } = data;
-    let type;
-
-    if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
-      type = 'Community';
-    } else {
-      type = 'Standalone';
-    }
-
-    return { id, type, ...attributes };
+    return this.store.find('rental', params.rental_id);
   }
 }
